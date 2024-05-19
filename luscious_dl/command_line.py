14521ff2f941975bc296d2e656335343e8db3917
shiny-comic <yooichin@gmail.com>
2024-05-19 13:38:49 +0900

    [style] formatting for PEP8

--- luscious_dl/command_line.py
+++ luscious_dl/command_line.py
@@ -4,55 +4,62 @@ from pathlib import Path
 
 
 def command_line() -> argparse.Namespace:
-  """
-  Defines the command line interface.
-  :return: arguments
-  """
-  parser = argparse.ArgumentParser(prog='Luscious Downloader', description='Download albums from luscious.net')
+    """
+    Defines the command line interface.
+    :return: arguments
+    """
+    parser = argparse.ArgumentParser(
+        prog='Luscious Downloader', description='Download albums from luscious.net')
 
-  group = parser.add_mutually_exclusive_group(required=True)
-  group.add_argument('--album', '-a', dest='album_inputs', action='store', help='download album by url or id')
-  group.add_argument('--user', '-u', dest='user_inputs', action='store', help='download all user albums by url or id')
-  group.add_argument('--search', '-s', dest='keyword', action='store', help='search albums by keyword')
-  group.add_argument('--list', '-l', dest='read_list', action='store_true',
-                     help="read a file list.txt in the folder you're running this command")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--album', '-a', dest='album_inputs',
+                       action='store', help='download album by url or id')
+    group.add_argument('--user', '-u', dest='user_inputs',
+                       action='store', help='download all user albums by url or id')
+    group.add_argument('--search', '-s', dest='keyword',
+                       action='store', help='search albums by keyword')
+    group.add_argument('--list', '-l', dest='read_list', action='store_true',
+                       help="read a file list.txt in the folder you're running this command")
 
-  # user args
-  parser.add_argument('--favorites', '-f', dest='only_favorites', action='store_true',
-                      help='download only user favorites')
-  parser.add_argument('--group', '--agroup', '-g', dest='group_by_user', action='store_true',
-                      help='when downloading user albums they will be grouped by name')
+    # user args
+    parser.add_argument('--favorites', '-f', dest='only_favorites', action='store_true',
+                        help='download only user favorites')
+    parser.add_argument('--group', '--agroup', '-g', dest='group_by_user', action='store_true',
+                        help='when downloading user albums they will be grouped by name')
 
-  # search args
-  parser.add_argument('--download', '-d', dest='search_download', action='store_true',
-                      help='download albums from search results')
-  parser.add_argument('--page', dest='page', action='store', type=int, default=1, help='page number of search results')
-  parser.add_argument('--max-page', dest='max_pages', action='store', type=int, default=1,
-                      help='max pages of search results')
-  parser.add_argument('--sorting', dest='sorting', action='store', default='date_trending',
-                      help='search sorting', choices=['date_trending', 'rating_all_time'])
+    # search args
+    parser.add_argument('--download', '-d', dest='search_download', action='store_true',
+                        help='download albums from search results')
+    parser.add_argument('--page', dest='page', action='store',
+                        type=int, default=1, help='page number of search results')
+    parser.add_argument('--max-page', dest='max_pages', action='store', type=int, default=1,
+                        help='max pages of search results')
+    parser.add_argument('--sorting', dest='sorting', action='store', default='date_trending',
+                        help='search sorting', choices=['date_trending', 'rating_all_time'])
 
-  # generate options
-  parser.add_argument('--pdf', '-p', dest='gen_pdf', action='store_true',
-                      help='enable pdf file generation')
-  parser.add_argument('--cbz', '-c', dest='gen_cbz', action='store_true',
-                      help='enable cbz file generation')
-  parser.add_argument('--rm-origin-dir', dest='rm_origin_dir', action='store_true',
-                      help='remove downloaded album dir when generated PDF file.')
+    # generate options
+    parser.add_argument('--pdf', '-p', dest='gen_pdf', action='store_true',
+                        help='enable pdf file generation')
+    parser.add_argument('--cbz', '-c', dest='gen_cbz', action='store_true',
+                        help='enable cbz file generation')
+    parser.add_argument('--rm-origin-dir', dest='rm_origin_dir', action='store_true',
+                        help='remove downloaded album dir when generated PDF file.')
 
-  # downloader args
-  parser.add_argument('--threads', '-t', dest='threads', action='store', type=int, default=os.cpu_count(),
-                      help='how many download threads')
-  parser.add_argument('--retries', '-R', dest='retries', action='store', type=int, default=5, help='download retries')
-  parser.add_argument('--timeout', '-T', dest='timeout', action='store', type=int, default=30, help='download timeout')
-  parser.add_argument('--delay', '-D', dest='delay', action='store', type=int, default=0,
-                      help='delay between downloading multiple albums')
-  parser.add_argument('--format', dest='foldername_format', action='store', default='%t',
-                      help='format album folder name')
+    # downloader args
     parser.add_argument('--output', '-o', dest='output_dir', type=Path, action='store', default=Path.cwd(), help='output directory')
+    parser.add_argument('--threads', '-t', dest='threads', action='store', type=int, default=os.cpu_count(),
+                        help='how many download threads')
+    parser.add_argument('--retries', '-R', dest='retries',
+                        action='store', type=int, default=5, help='download retries')
+    parser.add_argument('--timeout', '-T', dest='timeout',
+                        action='store', type=int, default=30, help='download timeout')
+    parser.add_argument('--delay', '-D', dest='delay', action='store', type=int, default=0,
+                        help='delay between downloading multiple albums')
+    parser.add_argument('--format', dest='foldername_format', action='store', default='%t',
+                        help='format album folder name')
 
-  return parser.parse_args()
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-  command_line()
+    command_line()
--- luscious_dl/start.py
+++ luscious_dl/start.py
@@ -6,159 +6,186 @@ from luscious_dl.album import Album, search_albums, print_search
 from luscious_dl.command_line import command_line
 from luscious_dl.downloader import Downloader
 from luscious_dl.logger import logger
-from luscious_dl.parser import extract_ids_from_list, extract_album_id, extract_user_id
+from luscious_dl.parser import extract_ids_from_list, \
+        extract_album_id, extract_user_id
 from luscious_dl.user import User
-from luscious_dl.utils import info, format_foldername, generate_pdf, inputs_string_to_list, delete_folder, \
-  generate_cbz, read_list
+from luscious_dl.utils import info, format_foldername, \
+        generate_pdf, inputs_string_to_list, delete_folder, \
+        generate_cbz, read_list
 
 
-def albums_download(albums_ids: list[int], downloader: Downloader, output_dir: Path,
-                    foldername_format='%t', gen_pdf=False, gen_cbz=False, rm_origin_dir=False) -> None:
-  """
-  Start albums download process.
-  :param albums_ids: list of album ids
-  :param downloader: Downloader object
-  :param foldername_format: album folder name format
-  :param output_dir: Path where albums will be saved
-  :param gen_pdf: indicates whether to generate the pdf file
-  :param gen_cbz: indicates whether to generate the cbz file
-  :param rm_origin_dir: indicates whether the source folder will be deleted
-  """
-  for id_ in albums_ids:
-    album = Album(id_)
-    try:
-      if album.fetch_info():
-        formmatted_foldername = format_foldername(album, foldername_format)
-        album_folder = Path.joinpath(output_dir, formmatted_foldername)
-        album.show()
-        album.fetch_pictures()
-        # album.generate_metadata(album_folder)
-        album.download(downloader, album_folder)
-        if gen_pdf:
-          generate_pdf(output_dir, formmatted_foldername, album_folder)
-        if gen_cbz:
-          generate_cbz(output_dir, formmatted_foldername, album_folder)
-        if rm_origin_dir:
-          delete_folder(album_folder, formmatted_foldername)
-      else:
-        raise Exception('Album Information not found.')
-    except Exception as e:
-      logger.critical(f'Album: {id_} Error: {e}')
+def albums_download(albums_ids: list[int],
+                    downloader: Downloader, output_dir: Path,
+                    foldername_format='%t',
+                    gen_pdf=False, gen_cbz=False, rm_origin_dir=False) -> None:
+    """
+    Start albums download process.
+    :param albums_ids: list of album ids
+    :param downloader: Downloader object
+    :param foldername_format: album folder name format
+    :param output_dir: Path where albums will be saved
+    :param gen_pdf: indicates whether to generate the pdf file
+    :param gen_cbz: indicates whether to generate the cbz file
+    :param rm_origin_dir: indicates whether the source folder will be deleted
+    """
+    for id_ in albums_ids:
+        album = Album(id_)
+        try:
+            if album.fetch_info():
+                formmatted_foldername = format_foldername(
+                    album, foldername_format)
+                album_folder = output_dir.joinpath(formmatted_foldername)
+                album.show()
+                album.fetch_pictures()
+                # album.generate_metadata(album_folder)
+                album.download(downloader, album_folder)
+                if gen_pdf:
+                    generate_pdf(
+                        output_dir, formmatted_foldername, album_folder)
+                if gen_cbz:
+                    generate_cbz(
+                        output_dir, formmatted_foldername, album_folder)
+                if rm_origin_dir:
+                    delete_folder(album_folder, formmatted_foldername)
+            else:
+                raise Exception('Album Information not found.')
+        except Exception as e:
+            logger.critical(f'Album: {id_} Error: {e}')
 
 
-def users_download(users_ids: list[int], downloader: Downloader, output_dir: Path, foldername_format='%t',
-                   only_favorites=False, gen_pdf=False, gen_cbz=False, rm_origin_dir=False, group_by_user=False) -> \
-        None:
-  """
-  Start users download process.
-  :param users_ids: list of user ids
-  :param downloader: Downloader object
-  :param output_dir: Path where albums will be saved
-  :param foldername_format: album folder name format
-  :param only_favorites: defines if it's to download only the favorites
-  :param gen_pdf: whether to generate the pdf file
-  :param gen_cbz: whether to generate the cbz file
-  :param rm_origin_dir: indicates whether the source folder will be deleted
-  :param group_by_user:
-  """
-  for id_ in users_ids:
-    user = User(id_)
-    try:
-      if user.fetch_info():
-        user.fetch_albums(only_favorites)
-        user.show()
-        if group_by_user:
-          output_dir = output_dir.joinpath(os.path.normcase(user.name))
-        albums_download(user.albums_ids, downloader, output_dir, foldername_format, gen_pdf, gen_cbz, rm_origin_dir)
-      else:
-        raise Exception('User Information not found.')
-    except Exception as e:
-      logger.critical(f'User: {id_} Error: {e}')
+def users_download(
+        users_ids: list[int],
+        downloader: Downloader,
+        output_dir: Path,
+        foldername_format='%t',
+        only_favorites=False,
+        gen_pdf=False,
+        gen_cbz=False,
+        rm_origin_dir=False,
+        group_by_user=False
+        ) -> None:
+    """
+    Start users download process.
+    :param users_ids: list of user ids
+    :param downloader: Downloader object
+    :param output_dir: Path where albums will be saved
+    :param foldername_format: album folder name format
+    :param only_favorites: defines if it's to download only the favorites
+    :param gen_pdf: whether to generate the pdf file
+    :param gen_cbz: whether to generate the cbz file
+    :param rm_origin_dir: indicates whether the source folder will be deleted
+    :param group_by_user:
+    """
+    for id_ in users_ids:
+        user = User(id_)
+        try:
+            if user.fetch_info():
+                user.fetch_albums(only_favorites)
+                user.show()
+                if group_by_user:
+                    output_dir = output_dir.joinpath(
+                        os.path.normcase(user.name))
+                albums_download(user.albums_ids, downloader, output_dir,
+                                foldername_format, gen_pdf, gen_cbz, rm_origin_dir)
+            else:
+                raise Exception('User Information not found.')
+        except Exception as e:
+            logger.critical(f'User: {id_} Error: {e}')
 
 
 def normalize_args(args: Namespace) -> Namespace:
-  """
-  Fix possible args inconsistencies.
-  :param args: Namespace
-  :return: Fixed Namespace
-  """
-  if args.threads <= 0:
-    args.threads = os.cpu_count()
-    if not args.threads:
-      logger.warning('It was not possible to determine the number of CPUs in your system. '
-                     'Only one will be used, this will decrease the amount of downloads.')
-      args.threads = 1
-  if args.page <= 0:
-    args.page = 1
-  if args.max_pages <= 0:
-    args.max_pages = 1
-  if args.page > args.max_pages:
-    args.max_pages = args.page
+    """
+    Fix possible args inconsistencies.
+    :param args: Namespace
+    :return: Fixed Namespace
+    """
+    if isinstance(args.output_dir, str):
+        args.output_dir = Path(args.output_dir)
+    if args.threads <= 0:
+        args.threads = os.cpu_count()
+        if not args.threads:
+            logger.warning('It was not possible to determine the number of CPUs in your system. '
+                           'Only one will be used, this will decrease the amount of downloads.')
+            args.threads = 1
+    if args.page <= 0:
+        args.page = 1
+    if args.max_pages <= 0:
+        args.max_pages = 1
+    if args.page > args.max_pages:
+        args.max_pages = args.page
 
-  if args.read_list:
-    list_data = read_list(Path.cwd())
-    args.album_inputs = ','.join(list_data)
+    if args.read_list:
+        list_data = read_list(Path.cwd())
+        args.album_inputs = ','.join(list_data)
 
-  if args.only_favorites and not args.user_inputs:
-    logger.warn("You're passing --favorites/-f flag without any user input.")
-    args.only_favorites = False
+    if args.only_favorites and not args.user_inputs:
+        logger.warn(
+            "You're passing --favorites/-f flag without any user input.")
+        args.only_favorites = False
 
-  if args.gen_pdf and not args.album_inputs and not args.user_inputs and not args.search_download:
-    logger.warn("You're passing --pdf flag without any album/user input or search download.")
-    args.gen_pdf = False
+    if args.gen_pdf and not args.album_inputs and not args.user_inputs and not args.search_download:
+        logger.warn(
+            "You're passing --pdf flag without any album/user input or search download.")
+        args.gen_pdf = False
 
-  if args.gen_cbz and not args.album_inputs and not args.user_inputs and not args.search_download:
-    logger.warn("You're passing --cbz flag without any album/user input or search download.")
-    args.gen_cbz = False
+    if args.gen_cbz and not args.album_inputs and not args.user_inputs and not args.search_download:
+        logger.warn(
+            "You're passing --cbz flag without any album/user input or search download.")
+        args.gen_cbz = False
 
-  if args.rm_origin_dir and not args.gen_pdf and not args.gen_cbz:
-    logger.warn("You're passing --rm-origin-dir flag without any pdf or cbz generation. This would delete the album "
-                "after download")
-    args.rm_origin_dir = False
+    if args.rm_origin_dir and not args.gen_pdf and not args.gen_cbz:
+        logger.warn("You're passing --rm-origin-dir flag without any pdf or cbz generation. This would delete the album "
+                    "after download")
+        args.rm_origin_dir = False
 
-  args.keyword = args.keyword.strip() if args.keyword else None
+    args.keyword = args.keyword.strip() if args.keyword else None
 
-  if args.album_inputs:
-    inputs = inputs_string_to_list(args.album_inputs)
-    args.albums_ids = extract_ids_from_list(inputs, extract_album_id)
-  else:
-    args.albums_ids = None
+    if args.album_inputs:
+        inputs = inputs_string_to_list(args.album_inputs)
+        args.albums_ids = extract_ids_from_list(inputs, extract_album_id)
+    else:
+        args.albums_ids = None
 
-  if args.user_inputs:
-    inputs = inputs_string_to_list(args.user_inputs)
-    args.users_ids = extract_ids_from_list(inputs, extract_user_id)
-  else:
-    args.users_ids = None
+    if args.user_inputs:
+        inputs = inputs_string_to_list(args.user_inputs)
+        args.users_ids = extract_ids_from_list(inputs, extract_user_id)
+    else:
+        args.users_ids = None
 
-  return args
+    return args
 
 
 def start(args: Namespace = None) -> None:
-  """Start"""
-  if not args:
-    info()
-  args = normalize_args(args or command_line())
-  downloader = Downloader(args.threads, args.retries, args.timeout, args.delay)
+    """Start"""
+    if not args:
+        info()
+    args = normalize_args(args or command_line())
+    downloader = Downloader(args.threads, args.retries,
+                            args.timeout, args.delay)
 
-  if args.albums_ids:
-    albums_download(args.albums_ids, downloader, args.output_dir, args.foldername_format,
-                    args.gen_pdf, args.gen_cbz, args.rm_origin_dir)
-  elif args.users_ids:
-    users_download(args.users_ids, downloader, args.output_dir, args.foldername_format,
-                   args.only_favorites, args.gen_pdf, args.gen_cbz, args.rm_origin_dir, args.group_by_user)
-  elif args.keyword:
-    result = search_albums(args.keyword, args.sorting, args.page, args.max_pages)
-    print_search(result)
-    if args.search_download:
-      for album in result:
-        album.show()
-        album.fetch_pictures()
-        formmatted_foldername = format_foldername(album, args.foldername_format)
-        album_folder = Path.joinpath(args.output_dir, formmatted_foldername)
-        album.download(downloader, album_folder)
-        if args.gen_pdf:
-          generate_pdf(args.output_dir, formmatted_foldername, album_folder)
+    if args.albums_ids:
+        albums_download(args.albums_ids, downloader, args.output_dir, args.foldername_format,
+                        args.gen_pdf, args.gen_cbz, args.rm_origin_dir)
+    elif args.users_ids:
+        users_download(args.users_ids, downloader, args.output_dir, args.foldername_format,
+                       args.only_favorites, args.gen_pdf, args.gen_cbz, args.rm_origin_dir, args.group_by_user)
+    elif args.keyword:
+        result = search_albums(args.keyword, args.sorting,
+                               args.page, args.max_pages)
+        print_search(result)
+        if args.search_download:
+            for album in result:
+                album.show()
+                album.fetch_pictures()
+                formmatted_foldername = format_foldername(
+                    album, args.foldername_format)
+                album_folder = Path.joinpath(
+                    args.output_dir, formmatted_foldername)
+                album.download(downloader, album_folder)
+                if args.gen_pdf:
+                    generate_pdf(args.output_dir,
+                                 formmatted_foldername, album_folder)
 
 
 if __name__ == '__main__':
-  start()
+    start()
